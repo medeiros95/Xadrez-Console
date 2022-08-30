a182a9fc87a8aa9249ded9b199e8973abea8ad3c
Victor Medeiros <victor.allisson95@gmail.com>
2022-08-30 11:28:10 -0300

    Tratando erros com excecao personalizada

---
+++ xadrez-console/tabuleiro/TabuleiroException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro
+{
+    internal class TabuleiroException : Exception
+    {
+        public TabuleiroException(string msg) : base(msg)
+        {
+        }
+    }
+}
